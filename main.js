27895f08e740196d78f16ca7a1c9ef1cf7f8ac11
Hoang Long <1612352@student.hcmus.edu.vn>
2020-05-29 19:17:15 +0700

    Hàm khởi tạo block đầu tiên (Genesis block)

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-class 
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data).toString());
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "16/11/1998", "Genesis Block", "0");
+    }
+
+    getLastestBlock() {
+        
+    }
+}
